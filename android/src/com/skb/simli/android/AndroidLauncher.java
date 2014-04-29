f7f12143154becf607c88392554c5c6aa1e2945c
Surya <suryakumarbmail-libgdx@yahoo.co.in>
2014-04-29 09:49:15 +0530

    Initial commit

---
+++ android/src/com/skb/simli/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.skb.simli.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.skb.simli.SimLi;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SimLi(), config);
+	}
+}
---
+++ desktop/src/com/skb/simli/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skb.simli.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skb.simli.SimLi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimLi(), config);
+	}
+}
